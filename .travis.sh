2301f0d63e93dfbd849ccc87a53195ba3f93f583
Jacob Nerney <jnerney@kenzan.com>
2018-12-09 04:20:36 -0500

    deploy tests

--- .travis.sh
+++ .travis.sh
@@ -9,5 +9,5 @@ if [ "$TRAVIS_BRANCH" = develop ]; then
     docker push jnerney/riblet
     echo -e "$PEM" > riblet.pem
     chmod 400 riblet.pem
-    ssh -o StrictHostKeyChecking=no -i "riblet.pem" -t ec2-user@$HOST 'bash -s' < ls
+    ssh -o StrictHostKeyChecking=no -i "riblet.pem" -t ec2-user@$HOST 'bash -s' < docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD && docker pull jnerney/riblet && docker run jnerney/riblet
 fi
