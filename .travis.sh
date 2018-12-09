6200e931af8a5f7fd00e8f94f37d08d950ad126e
Jacob Nerney <jnerney@kenzan.com>
2018-12-09 04:15:24 -0500

    deploy tests

--- .travis.sh
+++ .travis.sh
@@ -9,5 +9,5 @@ if [ "$TRAVIS_BRANCH" = develop ]; then
     docker push jnerney/riblet
     echo -e "$PEM" > riblet.pem
     chmod 400 riblet.pem
-    ssh -o StrictHostKeyChecking=no -i "riblet.pem" -t ec2-user@$HOST 'bash -s' < ./redeploy.sh
+    ssh -o StrictHostKeyChecking=no -i "riblet.pem" -t ec2-user@$HOST 'bash -s' < "./redeploy.sh"
 fi
