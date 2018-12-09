05844fa2ebe3d590bbe6ae97cb8cac9307c159f5
Jacob Nerney <jnerney@kenzan.com>
2018-12-09 03:50:50 -0500

    deploy tests

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "net"
 
 func main() {
-	fmt.Println("riblet")
+	runServer()
+}
+
+func runServer() {
+	l, _ := net.Listen("tcp", ":80")
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				runServer()
+			}
+		}()
+		for {
+			c, err := l.Accept()
+			if err == nil {
+				go func(c net.Conn) {
+					c.Write([]byte("riblet"))
+				}(c)
+			}
+		}
+	}()
+
 }
